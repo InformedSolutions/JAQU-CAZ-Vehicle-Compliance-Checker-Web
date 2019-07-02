8aaa93cae3ea1d2ccb2dcd67b02d6eef65830722
Wojciech Mikusek <miqs1992@gmail.com>
2019-07-02 14:08:27 +0200

    [Fix] [VCCS-131] Fix input validation formats

--- app/models/vrn_form.rb
+++ app/models/vrn_form.rb
@@ -2,7 +2,7 @@
 
 class VrnForm < BaseForm
   def valid?
-    filled? && valid_format? && not_to_long? && not_to_short?
+    filled? && not_to_long? && not_to_short? && valid_format?
   end
 
   private
@@ -14,7 +14,7 @@ class VrnForm < BaseForm
 
   def valid_format?
     @message = 'You must enter your registration number in valid format'
-    parameter.match('^[a-zA-Z0-9\s]+$').present?
+    FORMAT_REGEXPS.any? { |reg| reg.match(parameter.gsub(/\s+/, '').upcase).present? }
   end
 
   def not_to_long?
@@ -26,4 +26,38 @@ class VrnForm < BaseForm
     @message = 'Your registration number is too short'
     parameter.gsub(/\s+/, '').length > 1
   end
+
+  FORMAT_REGEXPS = [
+    /^[A-Z]{3}[0-9]{3}$/, # AAA999
+    /^[A-Z][0-9]{3}[A-Z]{3}$/, # A999AAA
+    /^[A-Z]{3}[0-9]{3}[A-Z]$/, # AAA999A
+    /^[A-Z]{3}[0-9]{4}$/, # AAA9999
+    /^[A-Z]{2}[0-9]{2}[A-Z]{3}$/, # AA99AAA
+    /^[0-9]{4}[A-Z]{3}$/, # 9999AAA
+    /^[A-Z][0-9]$/, # A9
+    /^[0-9][A-Z]$/, # 9A
+    /^[A-Z]{2}[0-9]$/, # AA9
+    /^[A-Z][0-9]{2}$/, # A99
+    /^[0-9][A-Z]{2}$/, # 9AA
+    /^[0-9]{2}[A-Z]$/, # 99A
+    /^[A-Z]{3}[0-9]$/, # AAA9
+    /^[A-Z][0-9]{3}$/, # A999
+    /^[A-Z]{2}[0-9]{2}$/, # AA99
+    /^[0-9][A-Z]{3}$/, # 9AAA
+    /^[0-9]{2}[A-Z]{2}$/, # 99AA
+    /^[0-9]{3}[A-Z]$/, # 999A
+    /^[A-Z][0-9][A-Z]{3}$/, # A9AAA
+    /^[A-Z]{3}[0-9][A-Z]$/, # AAA9A
+    /^[A-Z]{3}[0-9]{2}$/, # AAA99
+    /^[A-Z]{2}[0-9]{3}$/, # AA999
+    /^[0-9]{2}[A-Z]{3}$/, # 99AAA
+    /^[0-9]{3}[A-Z]{2}$/, # 999AA
+    /^[0-9]{4}[A-Z]$/, # 9999A
+    /^[A-Z][0-9]{4}$/, # A9999
+    /^[A-Z][0-9]{2}[A-Z]{3}$/, # A99AAA
+    /^[A-Z]{3}[0-9]{2}[A-Z]$/, # AAA99A
+    /^[0-9]{3}[A-Z]{3}$/, # 999AAA
+    /^[A-Z]{2}[0-9]{4}$/, # AA9999
+    /^[0-9]{4}[A-Z]{2}$/ # 9999AA
+  ].freeze
 end
--- spec/models/vrn_form_spec.rb
+++ spec/models/vrn_form_spec.rb
@@ -7,20 +7,18 @@ RSpec.describe VrnForm, type: :model do
 
   let(:vrn) { 'CU57ABC' }
 
-  it 'is valid with a proper VRN' do
-    expect(form.valid?).to eq(true)
-  end
+  context 'with proper VRN' do
+    it { is_expected.to be_valid }
 
-  it 'has VRN set as parameter' do
-    expect(form.parameter).to eq(vrn)
+    it 'has VRN set as parameter' do
+      expect(form.parameter).to eq(vrn)
+    end
   end
 
   context 'when VRN is empty' do
     let(:vrn) { '' }
 
-    it 'is not valid' do
-      expect(form.valid?).to eq(false)
-    end
+    it { is_expected.not_to be_valid }
 
     it 'has a proper error message' do
       expect(form.message).to eq('You must enter your registration number')
@@ -30,36 +28,310 @@ RSpec.describe VrnForm, type: :model do
   context 'when VRN is too long' do
     let(:vrn) { 'ABCDEFGH' }
 
-    it 'is not valid' do
-      expect(form.valid?).to eq(false)
-    end
+    it { is_expected.not_to be_valid }
 
     it 'has a proper error message' do
       expect(form.message).to eq('Your registration number is too long')
     end
   end
 
-  context 'when VRN is too long' do
+  context 'when VRN is too short' do
     let(:vrn) { 'A' }
 
-    it 'is not valid' do
-      expect(form.valid?).to eq(false)
-    end
+    it { is_expected.not_to be_valid }
 
     it 'has a proper error message' do
       expect(form.message).to eq('Your registration number is too short')
     end
   end
 
-  context 'when VRN has wrong format' do
+  context 'when VRN has special signs' do
     let(:vrn) { 'ABCDE$%' }
 
-    it 'is not valid' do
-      expect(form.valid?).to eq(false)
-    end
+    it { is_expected.not_to be_valid }
 
     it 'has a proper error message' do
       expect(form.message).to eq('You must enter your registration number in valid format')
+    end
+  end
+
+  context 'when VRN has too many numbers' do
+    let(:vrn) { 'C111999' }
+
+    it { is_expected.not_to be_valid }
+
+    it 'has a proper error message' do
+      expect(form.message).to eq('You must enter your registration number in valid format')
+    end
+  end
+
+  describe 'VRN formats' do
+    describe 'invalid formats' do
+      context 'when VRN is in format AA' do
+        let(:vrn) { 'AB' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format 99' do
+        let(:vrn) { '45' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format AAA' do
+        let(:vrn) { 'ABG' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format 999' do
+        let(:vrn) { '452' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format AAAA' do
+        let(:vrn) { 'TABG' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format 9999' do
+        let(:vrn) { '4521' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format AAAAA' do
+        let(:vrn) { 'TAFBG' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format 99999' do
+        let(:vrn) { '45921' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format AAAAAA' do
+        let(:vrn) { 'AHTDSE' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format A999A9' do
+        let(:vrn) { 'A123B5' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format 9999999' do
+        let(:vrn) { '4111929' }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when VRN is in format A9999A9' do
+        let(:vrn) { 'C1119C9' }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    context 'with spaces' do
+      context 'when VRN is in format AAA 999' do
+        let(:vrn) { 'ABC 123' }
+
+        it { is_expected.to be_valid }
+      end
+    end
+
+    context 'when VRN is in format AAA999' do
+      let(:vrn) { 'ABC123' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A999AAA' do
+      let(:vrn) { 'A123BCD' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AAA999A' do
+      let(:vrn) { 'GAD975C' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AAA9999' do
+      let(:vrn) { 'ZEA1436' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AA99AAA' do
+      let(:vrn) { 'SK12JKL' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 9999AAA' do
+      let(:vrn) { '7429HER' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A9' do
+      let(:vrn) { 'G5' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 9A' do
+      let(:vrn) { '6W' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AA9' do
+      let(:vrn) { 'JK4' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A99' do
+      let(:vrn) { 'P91' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 9AA' do
+      let(:vrn) { '9RA' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 99A' do
+      let(:vrn) { '81U' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AAA9' do
+      let(:vrn) { 'KAT7' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A999' do
+      let(:vrn) { 'Y478' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AA99' do
+      let(:vrn) { 'LK31' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 9AAA' do
+      let(:vrn) { '8RAD' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 99AA' do
+      let(:vrn) { '87KJ' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 999A' do
+      let(:vrn) { '111Z' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A9AAA' do
+      let(:vrn) { 'A7CUD' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AAA9A' do
+      let(:vrn) { 'VAR7A' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AAA99' do
+      let(:vrn) { 'FES23' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AA999' do
+      let(:vrn) { 'PG227' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 99AAA' do
+      let(:vrn) { '30JFA' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 999AA' do
+      let(:vrn) { '868BO' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 9999A' do
+      let(:vrn) { '1289J' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A9999' do
+      let(:vrn) { 'B8659' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format A99AAA' do
+      let(:vrn) { 'K97LUK' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AAA99A' do
+      let(:vrn) { 'MAN07U' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 999AAA' do
+      let(:vrn) { '546BAR' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format AA9999' do
+      let(:vrn) { 'JU0043' }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when VRN is in format 9999AA' do
+      let(:vrn) { '8839GF' }
+
+      it { is_expected.to be_valid }
     end
   end
 end
