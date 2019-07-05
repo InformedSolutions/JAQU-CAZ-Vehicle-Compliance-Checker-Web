f41bf2384da9416d5a068ba6ca90065933534e9a
Krzysztof Szromek <1729299+szromek@users.noreply.github.com>
2019-07-05 12:00:34 +0200

    [VCCS-168] Docker image for running tests and production release

--- config/initializers/env_variables_checker.rb
+++ config/initializers/env_variables_checker.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 if ENV['COMPLIANCE_CHECKER_API_URL'].blank?
-  raise('You need to set COMPLIANCE_CHECKER_API_URL variable in .env file.')
+  Rails.logger.debug('COMPLIANCE_CHECKER_API_URL environment variable not set')
 end
