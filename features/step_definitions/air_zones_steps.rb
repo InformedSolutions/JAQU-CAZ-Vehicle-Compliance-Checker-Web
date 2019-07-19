62fbfe30940298d32242bdf8b4d18acac2a4e26a
Sergii Chub <s.chub@exlabs.pl>
2019-07-19 12:42:22 +0200

    [NT] Remove no need includes from cucumber steps

--- features/step_definitions/air_zones_steps.rb
+++ features/step_definitions/air_zones_steps.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-include MockHelpers
-
 Then('I should see the CAZ selection page') do
   expect(page).to have_current_path(caz_selection_air_zones_path, ignore_query: true)
 end
