fedec1be6d4c436f317f99de8d2613c576abb760
Sergii Chub <s.chub@exlabs.pl>
2019-08-08 10:09:47 +0200

    fixup! [VCCS-433] Add static cookie page and update cookie banner

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :disabled
+Rails.application.config.session_store :cookie_store,
+                                       key: '_caz_session'
