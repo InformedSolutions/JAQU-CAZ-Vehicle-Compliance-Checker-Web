39f60744c6d9652cdfa61369a4588bce8e4b0d90
Sergii Chub <s.chub@exlabs.pl>
2019-08-09 14:33:43 +0200

    fixup! [VCCS-435] Remove vrn and zones from urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
   end
 
   def check_vrn
-    redirect_to enter_details_vehicle_checkers_path unless session[:vrn]
+    redirect_to enter_details_vehicle_checkers_path unless vrn
   end
 
   def vrn
