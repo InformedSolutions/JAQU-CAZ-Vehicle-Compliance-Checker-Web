94f18bae534f7d2fbe20a08281319ddff6dada21
Wojtek Mikusek <miqs1992@gmail.com>
2019-08-22 16:14:01 +0200

    [VCCS-459] Add content to non-UK vehicle page

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,9 @@ module JaquCaz
     feedback_url_default = 'https://www.surveymonkey.co.uk/r/2RNBKTV'
     config.x.feedback_url = (ENV['FEEDBACK_URL'].presence || feedback_url_default)
 
+    contact_email_default = 'something@something.com'
+    config.x.contact_email = (ENV['CONTACT_EMAIL'].presence || contact_email_default)
+
     # https://mattbrictson.com/dynamic-rails-error-pages
     config.exceptions_app = routes
   end
