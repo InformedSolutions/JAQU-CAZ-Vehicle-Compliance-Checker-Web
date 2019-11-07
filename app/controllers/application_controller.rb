759b3b40447ba2aca3fe95a2235334af5adbcacb
Sergii Chub <s.chub@exlabs.pl>
2019-11-07 15:30:27 +0200

    [VCCS-754] Add basic authentication (#199)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
                                password: ENV['HTTP_BASIC_PASSWORD'],
                                if: lambda {
-                                     !Rails.env.production? && ENV['HTTP_BASIC_PASSWORD'].present?
+                                     Rails.env.production? && ENV['HTTP_BASIC_PASSWORD'].present?
                                    }
 
   ##
