b67c4c9650e2b341d7b41de140bf166fab5d6435
Wojtek Mikusek <miqs1992@gmail.com>
2019-12-05 11:28:25 +0100

    [VCCS-895] Fix print link

--- app/javascript/src/printLink/init.js
+++ app/javascript/src/printLink/init.js
@@ -4,7 +4,11 @@ function init() {
   const printLink = document.getElementById('print-page-link');
   if (printLink)
   {
-    printLink.classList.remove('print-page-link__hidden')
+    printLink.classList.remove('print-page-link__hidden');
+    printLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      window.print();
+    }, false);
   }
 }
 
