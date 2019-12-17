bfd88141ef94af56cb0e2b5a43802678b96dde39
Sergii Chub <s.chub@exlabs.pl>
2019-12-17 10:41:16 +0200

    [CAZB-123] Handle 422 response in compliance endpoint (#263)

--- spec/requests/air_zones/compliance_spec.rb
+++ spec/requests/air_zones/compliance_spec.rb
@@ -7,21 +7,38 @@ RSpec.describe 'AirZonesController - GET #compliance', type: :request do
 
   let(:caz) { ['leeds'] }
 
-  before do
-    add_vrn_to_session
-    compliance = JSON.parse(file_fixture('vehicle_compliance_response.json').read)
-    allow(ComplianceCheckerApi).to receive(:vehicle_compliance).and_return(compliance)
-    http_request
+  before { add_vrn_to_session }
+
+  context 'when api returns 200 status' do
+    before do
+      compliance = JSON.parse(file_fixture('vehicle_compliance_response.json').read)
+      allow(ComplianceCheckerApi).to receive(:vehicle_compliance).and_return(compliance)
+      http_request
+    end
+
+    it 'returns an ok response' do
+      expect(response).to have_http_status(:ok)
+    end
   end
 
-  it 'returns an ok response' do
-    expect(response).to have_http_status(:ok)
+  context 'when api returns 422 status' do
+    before do
+      allow(ComplianceCheckerApi).to receive(:vehicle_compliance)
+        .and_raise(BaseApi::Error422Exception.new(422, '',
+                                                  'message' => 'Something went wrong'))
+      http_request
+    end
+
+    it 'redirects to unable to determine compliance page' do
+      expect(response).to redirect_to(cannot_determinate_vehicle_checkers_path)
+    end
   end
 
   context 'when form is invalid' do
     let(:caz) { [] }
 
     it 'redirects to caz selection page' do
+      http_request
       expect(response).to redirect_to(caz_selection_air_zones_path)
     end
   end
