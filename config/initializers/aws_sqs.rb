c6266d7168174c0e106c1670e9aa05325a1b42da
Sergii Chub <s.chub@exlabs.pl>
2020-02-10 12:36:15 +0100

    [NT] Remove sass-rails and small refactoring (#347)

--- config/initializers/aws_sqs.rb
+++ config/initializers/aws_sqs.rb
@@ -2,6 +2,6 @@
 
 AWS_SQS = Aws::SQS::Client.new(
   region: ENV.fetch('AWS_REGION', 'eu-west-2'),
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID'),
+  secret_access_key: ENV.fetch('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID')
 )
