e33616f0f3aeb70e08cf4a6a6827470cdc527fee
proctorj <james.proctor@informed.com>
2020-02-28 16:50:56 +0000

    Merged filtering for vrm and password

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,5 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
-Rails.application.config.filter_parameters += [:vrn]
+Rails.application.config.filter_parameters += %i[password vrn]
