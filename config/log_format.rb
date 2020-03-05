200d4482760ff4a5d22a19170fbe1de7e1ca2bf6
proctorj <james.proctor@informed.com>
2020-03-05 13:22:55 +0000

    Added missing require logger call

--- config/log_format.rb
+++ config/log_format.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 # Modified Formatter class to remove IPs from logging - CAZSM8
 class FilteredFormatter < ::Logger::Formatter
-
   # https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
   IP_REGEX = /\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/.freeze
   FILTERED_STRING = '[REDACTED]'
 
   def call(severity, time, progname, msg)
-    formatted_severity = sprintf("%-5s",severity.to_s)
-    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    formatted_severity = format('%<sev>-5s', sev: severity.to_s)
+    formatted_time = time.strftime('%Y-%m-%d %H:%M:%S')
     formatted_msg = filter_pii(msg)
-    "[#{formatted_severity} #{formatted_time} #{progname}] #{formatted_msg}\n"
+    "[#{formatted_severity} #{formatted_time}] #{progname} #{formatted_msg}\n"
   end
 
   # If log string contains IP address then replace it with custom string
