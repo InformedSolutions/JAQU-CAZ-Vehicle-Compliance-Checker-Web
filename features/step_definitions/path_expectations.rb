0826f9d19136e6e619c24b850d99dd2e2932919f
Sergii Chub <s.chub@exlabs.pl>
2020-03-09 16:53:05 +0100

    [CAZB-723] Update enter details page

---
+++ features/step_definitions/path_expectations.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Then('I should be on the enter details page') do
+  expect_path(enter_details_vehicle_checkers_path)
+end
+
+private
+
+def expect_path(path)
+  expect(page).to have_current_path(path)
+end
