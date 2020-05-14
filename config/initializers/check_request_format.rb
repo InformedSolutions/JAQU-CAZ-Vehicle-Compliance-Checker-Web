cc856009dadfc9ff6e6dbecea2a9e0d07c729474
Sergii Chub <s.chub@exlabs.pl>
2020-05-14 15:22:49 +0200

    Fix loading fonts (#446)

--- config/initializers/check_request_format.rb
+++ config/initializers/check_request_format.rb
@@ -4,7 +4,6 @@
 # if the request should be rejected. Used in routes.rb
 class CheckRequestFormat
   def self.matches?(request)
-    Rails.logger.warn("Invalid request format: #{request.format.symbol}")
-    %i[html json xml woff].include?(request.format.symbol)
+    %i[html json xml].include?(request.format.symbol)
   end
 end
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,21 +6,21 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# if Rails.env.production?
-#   defaults = %i[self https]
-#   defaults.push("https://#{ENV['CLOUDFRONT_ENDPOINT']}") if ENV['CLOUDFRONT_ENDPOINT']
-#
-#   Rails.application.config.content_security_policy do |policy|
-#     policy.default_src(:none)
-#     policy.font_src(*defaults, :data)
-#     policy.img_src(*defaults)
-#     policy.object_src(:none)
-#     policy.script_src(*defaults, :unsafe_inline)
-#     policy.style_src(*defaults, :unsafe_inline)
-#     policy.connect_src(*defaults)
-#     policy.frame_ancestors(:none)
-#   end
-# end
+if Rails.env.production?
+  defaults = %i[self https]
+  defaults.push(ENV['CLOUDFRONT_ENDPOINT']) if ENV['CLOUDFRONT_ENDPOINT']
+
+  Rails.application.config.content_security_policy do |policy|
+    policy.default_src(:none)
+    policy.font_src(*defaults, :data)
+    policy.img_src(*defaults)
+    policy.object_src(:none)
+    policy.script_src(*defaults, :unsafe_inline)
+    policy.style_src(*defaults, :unsafe_inline)
+    policy.connect_src(*defaults)
+    policy.frame_ancestors(:none)
+  end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator =
