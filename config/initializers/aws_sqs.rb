77ae6f4366bb567b203a1d00f93fb43e1f45975d
Piotr Kałamarski <pkalamarski@gmail.com>
2020-06-04 13:00:28 +0200

    change

--- config/initializers/aws_sqs.rb
+++ config/initializers/aws_sqs.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+creds = if Rails.env.production?
+          Aws::ECSCredentials.new(ip_address: '169.254.170.2')
+        else
+          Aws::Credentials.new(
+            ENV.fetch('S3_AWS_ACCESS_KEY_ID', 'S3_AWS_ACCESS_KEY_ID'),
+            ENV.fetch('S3_AWS_SECRET_ACCESS_KEY', 'S3_AWS_SECRET_ACCESS_KEY')
+          )
+        end
+
 AWS_SQS = Aws::SQS::Client.new(
   region: ENV.fetch('AWS_REGION', 'eu-west-2'),
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  credentials: creds
 )
