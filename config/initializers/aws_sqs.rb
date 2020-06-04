4c08fe5b76cd0bf990feff25dce81f61f6375430
Piotr Kałamarski <pkalamarski@gmail.com>
2020-06-04 14:19:45 +0200

    fix envs

--- config/initializers/aws_sqs.rb
+++ config/initializers/aws_sqs.rb
@@ -4,8 +4,8 @@ creds = if Rails.env.production?
           Aws::ECSCredentials.new(ip_address: '169.254.170.2')
         else
           Aws::Credentials.new(
-            ENV.fetch('S3_AWS_ACCESS_KEY_ID', 'S3_AWS_ACCESS_KEY_ID'),
-            ENV.fetch('S3_AWS_SECRET_ACCESS_KEY', 'S3_AWS_SECRET_ACCESS_KEY')
+            ENV['AWS_ACCESS_KEY_ID'],
+            ENV['AWS_SECRET_ACCESS_KEY']
           )
         end
 
