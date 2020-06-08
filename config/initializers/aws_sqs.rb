8de78c77eae2a96b7032daa794c75ee6c2d88bc1
Piotr Kałamarski <pkalamarski@gmail.com>
2020-06-08 10:18:49 +0200

    add if env exists condition

--- config/initializers/aws_sqs.rb
+++ config/initializers/aws_sqs.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-creds = if Rails.env.production?
-          Aws::ECSCredentials.new(ip_address: '169.254.170.2')
-        else
+creds = if ENV['AWS_ACCESS_KEY_ID'] || !Rails.env.production?
           Aws::Credentials.new(
             ENV['AWS_ACCESS_KEY_ID'],
             ENV['AWS_SECRET_ACCESS_KEY']
           )
+        else
+          Aws::ECSCredentials.new({ ip_address: '169.254.170.2' })
         end
 
 AWS_SQS = Aws::SQS::Client.new(
