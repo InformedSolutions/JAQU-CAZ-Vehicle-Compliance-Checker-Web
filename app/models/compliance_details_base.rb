4540f0105a15b1fdeb3f0f30ab459536ded56bac
Sergii Chub <s.chub@exlabs.pl>
2021-02-11 17:23:35 +0100

    [CAZB-3953] Update results page to include link to payments

--- app/models/compliance_details_base.rb
+++ app/models/compliance_details_base.rb
@@ -3,16 +3,25 @@
 ##
 # This module is used to display data in +app/views/air_zones/compliance.html.haml+.
 module ComplianceDetailsBase
-  # Depend on CAZ name returns proper date
+  # Depend on CAZ name and charging date returns a proper value
   # Returns a string, e.g. '15 March 2021'
   def charging_starts
     case zone_name
     when 'Bath'
-      '15 March 2021'
+      date = '15 March 2021'
+      caz_live_in_future?(date) ? date : 'Now'
     when 'Birmingham'
-      '1 June 2021'
+      date = '1 June 2021'
+      caz_live_in_future?(date) ? date : 'Now'
     else
       'Early 2021'
     end
+  end
+
+  private
+
+  # Checks if a date charging is live
+  def caz_live_in_future?(date)
+    Date.parse(date).future?
   end
 end
