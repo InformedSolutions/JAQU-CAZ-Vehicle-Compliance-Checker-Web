81c25a1e1874835240797aadfc7dff422b5bc7af
Sergii Chub <s.chub@exlabs.pl>
2021-06-15 07:38:24 +0200

    [CAZB-4190] Encode a vrn to handle non-ascii characters (#766)

--- app/api_wrapper/compliance_checker_api.rb
+++ app/api_wrapper/compliance_checker_api.rb
@@ -48,7 +48,7 @@ class ComplianceCheckerApi < BaseApi
     #
     def vehicle_details(vrn)
       log_action 'Making request for vehicle details with VRN'
-      request(:get, "/vehicles/#{vrn}/details")
+      request(:get, "/vehicles/#{CGI.escape(vrn)}/details")
     end
 
     ##
@@ -95,7 +95,7 @@ class ComplianceCheckerApi < BaseApi
     #
     def vehicle_compliance(vrn)
       log_action 'Making request for vehicle compliance in all zones'
-      request(:get, "/vehicles/#{vrn}/compliance")
+      request(:get, "/vehicles/#{CGI.escape(vrn)}/compliance")
     end
 
     ##
@@ -132,7 +132,7 @@ class ComplianceCheckerApi < BaseApi
     #
     def register_details(vrn)
       log_action 'Making request for vehicle presence on specific registers'
-      request(:get, "/vehicles/#{vrn}/register-details")
+      request(:get, "/vehicles/#{CGI.escape(vrn)}/register-details")
     end
 
     ##
